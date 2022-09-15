a25608645ade1135e257a43943b4e697a2e3d72a
fstellato <fabrizio.stellato@olivetti.com>
2022-09-15 09:51:48 +0200

    improved tests

--- src/test/java/it/stellato/cgm/PatientsResourceTest.java
+++ src/test/java/it/stellato/cgm/PatientsResourceTest.java
@@ -12,8 +12,17 @@ import org.mockito.Mockito;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.Year;
+
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
@@ -26,11 +35,12 @@ public class PatientsResourceTest {
     Logger logger;
 
     @Test
-    public void testCreatePatientEndpoint() {
+    public void testCreatePatientEndpoint() throws Exception {
 
         final var patient = new Patient();
         patient.setName("Mario");
         patient.setSurname("Rossi");
+        patient.setBirthDate(LocalDate.now().minus(Period.ofYears(20)));
         patient.setSocialSecurityNumber("AA123456Z");
 
         Mockito.when(repository.add(Mockito.isA(Patient.class))).then(i -> {
@@ -39,10 +49,16 @@ public class PatientsResourceTest {
             return patient;
         });
 
+        final String body;
+
+        try (Jsonb jsonb = JsonbBuilder.create()) {
+            body = jsonb.toJson(patient);
+        }
+        
         given()
                 .contentType(ContentType.JSON)
                 .and()
-                .body(patient)
+                .body(body)
                 .when()
                 .post("/patients")
                 .then()
